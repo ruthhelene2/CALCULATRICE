0ab9a833c1704d36ebd344bacee6ff98c2bbeb85
ruthhelene2 <ruthhelene2@gmail.com>
2022-01-17 15:35:54 +0100

    version finale

---
+++ Calculatrice/TestCalco.java
@@ -0,0 +1,14 @@
+package com.LABO.Calculatrice;
+
+public class TestCalco {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			//Calculatrice casio = new Calculatrice();
+		     // CalculatriceN casio = new CalculatriceN();
+			 Calculatrice casio = new Calculatrice();
+
+		}
+
+	}
+
